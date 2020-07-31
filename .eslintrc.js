c1bcbccc658ebd059738b8bd7e3d0b6b465b9097
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-07-31 19:56:42 -0300

    Added eslint and editor extra settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
     // Base ESLint recommended rules
     // 'eslint:recommended',
 
-
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
@@ -28,14 +27,12 @@ module.exports = {
     // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
     'standard'
-
   ],
 
   plugins: [
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
     // required to lint *.vue files
-    'vue',
-
+    'vue'
   ],
 
   globals: {
@@ -49,12 +46,72 @@ module.exports = {
 
   // add your custom rules here
   rules: {
+    'space-before-function-paren': 0,
+    'constructor-super': 2,
+    'for-direction': 2,
+    'getter-return': 2,
+    'no-async-promise-executor': 2,
+    'no-case-declarations': 2,
+    'no-class-assign': 2,
+    'no-compare-neg-zero': 2,
+    'no-cond-assign': 2,
+    'no-const-assign': 2,
+    'no-constant-condition': 2,
+    'no-control-regex': 2,
+    'no-debugger': 2,
+    'no-delete-var': 2,
+    'no-dupe-args': 2,
+    'no-dupe-class-members': 2,
+    'no-dupe-else-if': 2,
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    'no-empty': 2,
+    'no-empty-character-class': 2,
+    'no-empty-pattern': 2,
+    'no-ex-assign': 2,
+    'no-extra-boolean-cast': 2,
+    'no-extra-semi': 2,
+    'no-fallthrough': 2,
+    'no-func-assign': 2,
+    'no-global-assign': 2,
+    'no-import-assign': 2,
+    'no-inner-declarations': 2,
+    'no-invalid-regexp': 2,
+    'no-irregular-whitespace': 2,
+    'no-misleading-character-class': 2,
+    'no-mixed-spaces-and-tabs': 2,
+    'no-new-symbol': 2,
+    'no-obj-calls': 2,
+    'no-octal': 2,
+    'no-prototype-builtins': 2,
+    'no-redeclare': 2,
+    'no-regex-spaces': 2,
+    'no-self-assign': 2,
+    'no-setter-return': 2,
+    'no-shadow-restricted-names': 2,
+    'no-sparse-arrays': 2,
+    'no-this-before-super': 2,
+    'no-undef': 2,
+    'no-unexpected-multiline': 2,
+    'no-unreachable': 2,
+    'no-unsafe-finally': 2,
+    'no-unsafe-negation': 2,
+    'no-unused-labels': 2,
+    'no-unused-vars': 2,
+    'no-useless-catch': 2,
+    'no-useless-escape': 2,
+    'no-with': 2,
+    'require-yield': 2,
+    'use-isnan': 2,
+    'valid-typeof': 2,
+    quotes: 1,
+    semi: 0,
+
     // allow async-await
     'generator-star-spacing': 'off',
     // allow paren-less arrow functions
     'arrow-parens': 'off',
     'one-var': 'off',
-
     'import/first': 'off',
     'import/named': 'error',
     'import/namespace': 'error',
@@ -65,8 +122,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'prefer-promise-reject-errors': 'off',
 
-
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
