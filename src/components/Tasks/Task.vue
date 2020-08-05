7466abdd7a8bf8d1dc902568d25985f048288068
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-05 16:08:06 -0300

    Breaking down code in components - creating and using the Task component

---
+++ src/components/Tasks/Task.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-item
+    v-on:click="task.completed = !task.completed"
+    :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+    clickable
+    v-ripple
+  >
+    <q-item-section side top>
+      <q-checkbox v-model="task.completed" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label :class="{ 'text-strikethrough': task.completed }">
+        {{ task.name }}
+      </q-item-label>
+    </q-item-section>
+
+    <q-item-section side top>
+      <div class="row">
+        <div class="column justify-center">
+          <q-icon name="event" size="18px" class="q-mr-xs" />
+        </div>
+        <div class="column">
+          <q-item-label caption class="row justify-end">
+            {{ task.dueDate }}
+          </q-item-label>
+          <q-item-label caption class="row justify-end">
+            <small> {{ task.dueTime }} </small>
+          </q-item-label>
+        </div>
+      </div>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  props: ['task', 'id'] // 'id' prop. is created for handling update and delete operations later on.
+};
+</script>
+
+<style></style>
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,56 +1,32 @@
 <template>
   <q-page class="q-pa-md">
     <q-list bordered separator>
-      <q-item
+      <app-task
         v-for="(task, key) in getTasks"
         :key="key"
-        v-on:click="task.completed = !task.completed"
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-        clickable
-        v-ripple
-      >
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label :class="{ 'text-strikethrough': task.completed }">
-            {{ task.name }}
-          </q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon name="event" size="18px" class="q-mr-xs" />
-            </div>
-            <div class="column">
-              <q-item-label caption class="row justify-end">
-                {{ task.dueDate }}
-              </q-item-label>
-              <q-item-label caption class="row justify-end">
-                <small> {{ task.dueTime }} </small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+        :task="task"
+        :id="key"
+      ></app-task>
     </q-list>
   </q-page>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import Task from 'components/Tasks/Task.vue';
 
 export default {
   computed: {
     ...mapGetters('moduleTasks', ['getTasks'])
     /*
-      // another way to do this, and change the getTasks by tasks in the v-for at line 5
+      // another way to do this (to use the tasks computed prop. change the "getTasks" to "tasks" in the v-for at line 5)
       tasks: function() {
         return this.$store.getters['moduleTasks/getTasks']
       }
     */
+  },
+  components: {
+    appTask: Task
   }
 };
 </script>
