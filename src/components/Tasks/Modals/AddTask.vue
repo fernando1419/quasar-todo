2b1071e7284bdee617d7761fe21d428243da6d59
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-08 21:44:48 -0300

    Added needed fields to the form and validating task name field

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -5,19 +5,97 @@
       <q-space></q-space>
       <q-btn flat dense round icon="close" v-close-popup />
     </q-card-section>
-    <q-card-section class="q-pt-none">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus
-      sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam,
-      ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
-    </q-card-section>
-    <q-card-actions align="right">
-      <q-btn flat label="Save" color="primary" v-close-popup />
-    </q-card-actions>
+
+    <!-- Form to Add a Task -->
+    <form v-on:submit="submitForm()">
+      <q-card-section class="q-pt-none">
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            v-model="taskToSubmit.name"
+            label="Task name"
+            hint="Provide a task name"
+            dense
+            :rules="[val => (val && val.length > 0) || 'Field is required.']"
+            ref="name"
+            class="col"
+          />
+        </div>
+
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            v-model="taskToSubmit.dueDate"
+            dense
+            label="Due date"
+            hint="Provide a Due Date"
+          >
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy
+                  ref="qDateProxy"
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-date v-model="taskToSubmit.dueDate" />
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
+
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            v-model="taskToSubmit.dueTime"
+            dense
+            label="Due time"
+            hint="Provide a Due Time"
+          >
+            <template v-slot:append>
+              <q-icon name="access_time" class="cursor-pointer">
+                <q-popup-proxy transition-show="scale" transition-hide="scale">
+                  <q-time v-model="taskToSubmit.dueTime" />
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
+      </q-card-section>
+
+      <!-- Button to save the task -->
+      <q-card-actions align="right">
+        <q-btn label="Save" color="primary" type="submit" />
+      </q-card-actions>
+    </form>
   </q-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      taskToSubmit: {
+        name: '',
+        dueDate: '',
+        dueTime: '',
+        completed: false
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      // console.log('Submitting form..');
+      this.$refs.name.validate(); // validates task name field.
+      if (!this.$refs.name.hasError) {
+        this.submitTask();
+      }
+    },
+    submitTask() {
+      console.log('submitting task..');
+    }
+  }
+};
 </script>
 
 <style></style>
